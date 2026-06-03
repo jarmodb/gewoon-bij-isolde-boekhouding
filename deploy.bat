7c20cc2c635e68966c976091ee9ac4d4bc5008b6
Jarmo De Blaeij <jarmo@arfyes.com>
2026-06-03 21:45:17 +0200

    Deploy via Vercel ipv Netlify

--- deploy.bat
+++ deploy.bat
@@ -3,7 +3,7 @@ cd /d "%~dp0"
 echo Bouwen...
 call npm run build
 echo.
-echo Deployen naar Netlify...
-call npx netlify-cli deploy --prod --dir dist
+echo Deployen naar Vercel...
+call npx vercel --prod
 echo.
 pause
